abaa47b11b11bb6d38fba34417b542b2e1b9ca56
Abel <86603742+abelwu2001@users.noreply.github.com>
2025-02-05 22:34:32 -0300

    Update secuencias.c
    ss

--- programa_principal/secuencias.c
+++ programa_principal/secuencias.c
@@ -2,7 +2,7 @@
 #include <ncurses.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <string.h>
+#include <time.h>
 
 #define LED1 24
 #define LED2 25
@@ -13,12 +13,14 @@
 #define LED7 20
 #define LED8 21
 
+// Función para convertir un número a una representación binaria en los LEDs
 void itob(int numero, int *leds) {
     for (int i = 0; i < 8; i++) {
         leds[i] = (numero >> (7 - i)) & 1;
     }
 }
 
+// Función para actualizar los LEDs físicos
 void interfaz(int *leds) {
     gpioWrite(LED1, leds[0]);
     gpioWrite(LED2, leds[1]);
@@ -30,168 +32,171 @@ void interfaz(int *leds) {
     gpioWrite(LED8, leds[7]);
 }
 
-// Función base para ejecutar una secuencia con control remoto
+// Función genérica para ejecutar secuencias
 void ejecutar_secuencia(void (*secuencia)(int *), int *velocidad) {
-    int ch;
-    initscr();
-    raw();
-    noecho();
-    keypad(stdscr, TRUE);
-    timeout(1);
+    gpioInitialise();
+    secuencia(velocidad);
+    gpioTerminate();
+}
 
-    while ((ch = getch()) != KEY_F(2)) { // Salir con F2 desde el maestro
-        if (ch == KEY_UP) {
-            *velocidad = (*velocidad > 100000) ? *velocidad - 50000 : *velocidad;
-        } else if (ch == KEY_DOWN) {
-            *velocidad = (*velocidad < 1000000) ? *velocidad + 50000 : *velocidad;
+// 🔹 **Secuencia "Auto Fantástico"**
+void secuencia_auto_fantastico_real(int *velocidad) {
+    int numero = 1;
+    int leds[8] = {0};
+    int direccion = 1;
+
+    while (1) {
+        itob(numero, leds);
+        interfaz(leds);
+
+        if (direccion == 1) {
+            if (numero < 128) numero <<= 1;
+            else direccion = -1;
+        } else {
+            if (numero > 1) numero >>= 1;
+            else direccion = 1;
         }
-        secuencia(velocidad);
+        gpioDelay(*velocidad);
     }
-
-    endwin();
 }
 
-// Secuencia "Auto Fantástico"
 void secuencia_auto_fantastico(int *velocidad) {
-    ejecutar_secuencia([](int *vel) {
-        int numero = 1;
-        int leds[8] = {0};
-        int direccion = 1;
-
-        while (1) {
-            itob(numero, leds);
-            interfaz(leds);
-
-            if (direccion == 1) {
-                numero = (numero < 128) ? numero << 1 : 64;
-                if (numero == 64) direccion = -1;
-            } else {
-                numero = (numero > 1) ? numero >> 1 : 2;
-                if (numero == 2) direccion = 1;
-            }
-            gpioDelay(*vel);
-        }
-    }, velocidad);
+    ejecutar_secuencia(secuencia_auto_fantastico_real, velocidad);
 }
 
-// Secuencia "El choque"
-void secuencia_choque(int *velocidad) {
-    ejecutar_secuencia([](int *vel) {
-        int leds[8] = {0};
-
-        while (1) {
-            for (int i = 0, j = 7; i < 4; i++, j--) {
-                memset(leds, 0, sizeof(leds));
-                leds[i] = 1;
-                leds[j] = 1;
-                interfaz(leds);
-                gpioDelay(*vel);
-            }
-            for (int i = 4, j = 3; i < 8; i++, j--) {
-                memset(leds, 0, sizeof(leds));
-                leds[i] = 1;
-                leds[j] = 1;
-                interfaz(leds);
-                gpioDelay(*vel);
-            }
-        }
-    }, velocidad);
-}
-
-// Secuencia "La apilada"
-void secuencia_apilada(int *velocidad) {
-    ejecutar_secuencia([](int *vel) {
-        int leds[8] = {0};
-
-        for (int limit = 0; limit < 8; limit++) {
-            for (int i = 7; i >= limit; i--) {
-                memset(leds, 0, sizeof(leds));
-                leds[i] = 1;
-                interfaz(leds);
-                gpioDelay(*vel);
-            }
-        }
-    }, velocidad);
-}
-
-// Secuencia "Carrera"
-void secuencia_carrera(int *velocidad) {
-    ejecutar_secuencia([](int *vel) {
-        int leds[8] = {0};
-        int pos1 = 0, pos2 = -1;
-
-        while (1) {
-            memset(leds, 0, sizeof(leds));
-            leds[pos1] = 1;
-            if (pos2 >= 0) leds[pos2] = 1;
-
-            interfaz(leds);
-            gpioDelay(*vel);
-
-            pos1 = (pos1 + 1) % 8;
-            if (pos1 == 4) pos2 = 0;
-            if (pos2 >= 0) pos2 = (pos2 + 2) % 8;
-        }
-    }, velocidad);
-}
-
-// Secuencia "Escalera"
-void secuencia_escalera(int *velocidad) {
-    ejecutar_secuencia([](int *vel) {
-        int leds[8] = {0};
-
-        for (int i = 0; i < 8; i++) {
+// 🔹 **Secuencia "Choque"**
+void secuencia_choque_real(int *velocidad) {
+    int leds[8] = {0};
+    while (1) {
+        for (int i = 0, j = 7; i < 4 && j >= 4; i++, j--) {
             leds[i] = 1;
+            leds[j] = 1;
             interfaz(leds);
-            gpioDelay(*vel);
-        }
-    }, velocidad);
-}
-
-// Secuencia "Chispas"
-void secuencia_chispas(int *velocidad) {
-    ejecutar_secuencia([](int *vel) {
-        int leds[8] = {0};
-        srand(time(NULL));
-
-        while (1) {
-            memset(leds, 0, sizeof(leds));
-            leds[rand() % 8] = 1;
-            interfaz(leds);
-            gpioDelay(*vel);
-        }
-    }, velocidad);
-}
-
-// Secuencia "Sirena"
-void secuencia_sirena(int *velocidad) {
-    ejecutar_secuencia([](int *vel) {
-        int tabla_sirena[8] = {0b10101010, 0b01010101, 0b11110000, 0b00001111,
-                               0b11111100, 0b00000011, 0b10000000, 0b00000001};
-        int leds[8];
-
-        for (int i = 0; i < 8; i++) {
-            itob(tabla_sirena[i], leds);
-            interfaz(leds);
-            gpioDelay(*vel);
-        }
-    }, velocidad);
-}
-
-// Secuencia "Matrix"
-void secuencia_matrix(int *velocidad) {
-    ejecutar_secuencia([](int *vel) {
-        int leds[8] = {0};
-
-        for (int i = 0; i < 8; i++) {
-            leds[i] = 1;
-            interfaz(leds);
-            gpioDelay(*vel);
-        }
-        for (int i = 0; i < 8; i++) {
+            gpioDelay(*velocidad);
             leds[i] = 0;
-            interfaz(leds);
-            gpioDelay(*vel);
+            leds[j] = 0;
         }
-    }, velocidad);
+    }
+}
+
+void secuencia_choque(int *velocidad) {
+    ejecutar_secuencia(secuencia_choque_real, velocidad);
+}
+
+// 🔹 **Secuencia "Apilada"**
+void secuencia_apilada_real(int *velocidad) {
+    int leds[8] = {0};
+    for (int limit = 0; limit < 8; limit++) {
+        for (int i = 7; i >= limit; i--) {
+            leds[i] = 1;
+            interfaz(leds);
+            gpioDelay(*velocidad);
+        }
+    }
+}
+
+void secuencia_apilada(int *velocidad) {
+    ejecutar_secuencia(secuencia_apilada_real, velocidad);
+}
+
+// 🔹 **Secuencia "Carrera"**
+void secuencia_carrera_real(int *velocidad) {
+    int leds[8] = {0};
+    int primera_pos = 0, segunda_pos = -1;
+    while (1) {
+        leds[primera_pos] = 1;
+        if (segunda_pos >= 0) leds[segunda_pos] = 1;
+
+        interfaz(leds);
+        gpioDelay(*velocidad);
+
+        leds[primera_pos] = 0;
+        if (segunda_pos >= 0) leds[segunda_pos] = 0;
+
+        primera_pos++;
+        if (primera_pos == 4) segunda_pos = 0;
+        if (segunda_pos >= 0) segunda_pos += 2;
+
+        if (primera_pos >= 8) {
+            primera_pos = 0;
+            segunda_pos = -1;
+        }
+    }
+}
+
+void secuencia_carrera(int *velocidad) {
+    ejecutar_secuencia(secuencia_carrera_real, velocidad);
+}
+
+// 🔹 **Secuencia "Escalera"**
+void secuencia_escalera_real(int *velocidad) {
+    int leds[8] = {0};
+    while (1) {
+        for (int i = 0; i < 8; i++) {
+            leds[i] = 1;
+            interfaz(leds);
+            gpioDelay(*velocidad);
+        }
+    }
+}
+
+void secuencia_escalera(int *velocidad) {
+    ejecutar_secuencia(secuencia_escalera_real, velocidad);
+}
+
+// 🔹 **Secuencia "Chispas"**
+void secuencia_chispas_real(int *velocidad) {
+    int leds[8] = {0};
+    srand(time(NULL));
+    while (1) {
+        for (int i = 0; i < 8; i++) leds[i] = 0;
+        leds[rand() % 8] = 1;
+        interfaz(leds);
+        gpioDelay(*velocidad);
+    }
+}
+
+void secuencia_chispas(int *velocidad) {
+    ejecutar_secuencia(secuencia_chispas_real, velocidad);
+}
+
+// 🔹 **Secuencia "Sirena"**
+void secuencia_sirena_real(int *velocidad) {
+    int leds[8] = {0};
+    int tabla_sirena[16][8] = {
+        {1, 0, 1, 0, 1, 0, 1, 0}, {0, 1, 0, 1, 0, 1, 0, 1},
+        {1, 1, 1, 1, 0, 0, 0, 0}, {0, 0, 0, 0, 1, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 0, 0}, {0, 0, 0, 0, 0, 0, 1, 1}
+    };
+    while (1) {
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 8; j++) leds[j] = tabla_sirena[i][j];
+            interfaz(leds);
+            gpioDelay(*velocidad);
+        }
+    }
+}
+
+void secuencia_sirena(int *velocidad) {
+    ejecutar_secuencia(secuencia_sirena_real, velocidad);
+}
+
+// 🔹 **Secuencia "Matrix"**
+void secuencia_matrix_real(int *velocidad) {
+    int leds[8] = {0};
+    int tabla_matrix[8][8] = {
+        {1, 0, 1, 0, 1, 0, 1, 0}, {0, 1, 0, 1, 0, 1, 0, 1},
+        {1, 1, 0, 1, 1, 0, 1, 1}, {0, 1, 1, 0, 1, 1, 0, 1}
+    };
+    while (1) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 8; j++) leds[j] = tabla_matrix[i][j];
+            interfaz(leds);
+            gpioDelay(*velocidad);
+        }
+    }
+}
+
+void secuencia_matrix(int *velocidad) {
+    ejecutar_secuencia(secuencia_matrix_real, velocidad);
 }
